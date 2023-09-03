dbd1a578c6aa6c28dc386d2a9512d82f7e405317
ghkdxofla <ghkdxofla@gmail.com>
2023-09-03 18:30:32 +0900

    feat: add a boot class

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -19,4 +19,10 @@ dependencies {
     implementation(project(":application"))
     implementation(project(":domain"))
     implementation(project(":infrastructure"))
+
+    /* spring */
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
 }
